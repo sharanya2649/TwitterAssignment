9fd9250f51db7b4a205a5ce051cdd3cc021820db
Sharanya <sharanya.m@techsophy.com>
2019-10-28 14:53:57 +0530

    first commit

--- src/main/scala/twitter/Tweet.scala
+++ src/main/scala/twitter/Tweet.scala
@@ -1,71 +1,81 @@
 package twitter
 
 
-import twitter4j._
 import slick.jdbc.MySQLProfile.api._
-import slick.lifted.QueryBase
-import slick.sql.FixedSqlAction
+import twitter4j._
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
 
+trait TwitterInstance {
+  val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+}
 
-object Tweet {
-  def main(args: Array[String]) {
-    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
-//    val p: Int =twitterStream.getConfiguration.getHttpProxyPort
-    twitterStream.addListener(Util.simpleStatusListener)
-    twitterStream.filter(new FilterQuery().track(Array("modi","smriti")).language(Array("en")))
-    Thread.sleep(4000)
-    twitterStream.cleanUp
-    twitterStream.shutdown
+object Tweet extends App with TwitterInstance {
+  twitterStream.addListener(Util.simpleStatusListener)
+  twitterStream.filter(new FilterQuery().track(Array("modi", "smriti")).language(Array("en")))
+  Thread.sleep(4000)
+  twitterStream.cleanUp()
+  twitterStream.shutdown()
+}
+
+class Twitter extends TwitterInstance {
+  def testFun: Int = twitterStream match {
+    case a: TwitterException => a.getStatusCode
+    case _ => 200
   }
 }
-class Twitter{
-  def testFun:Int={
-    val twitterStream2 = new TwitterStreamFactory(Util.config).getInstance
-    val p: Int =twitterStream2.getConfiguration.getHttpProxyPort
-    p
+
+class TwitterDetails {
+
+  val twitter = TableQuery(TwitterTable)
+
+  def insert(username: String, userTweet: String) = {
+    DatabaseConnection.db.run(twitter.map(t => (t.name, t.tweet)) += (username, userTweet))
   }
+
+  //  val twitterSchema: Unit =Await.result(DatabaseConnection.db.run(DBIO.seq(
+  //    twitter.schema.create
+  //  )), Duration.Inf)
+
+  case class TwitterTable(tag: Tag) extends Table[(String, String)](tag, "TWITTER_TABLE") {
+    def * = (name, tweet)
+
+    def name = column[String]("NAME")
+
+    def tweet = column[String]("TWEET")
+  }
+
 }
-class TwitterDetails{
-  case class TwitterTable(tag:Tag) extends Table[(String,String)](tag,"TWITTER_TABLE") {
-    def name= column[String]("NAME")
-    def tweet=column[String]("TWEET")
-    def * =(name,tweet)
-  }
-  val twitter= TableQuery(TwitterTable)
 
-//  val twitterSchema: Unit =Await.result(DatabaseConnection.db.run(DBIO.seq(
-//    twitter.schema.create
-//  )), Duration.Inf)
-
-
-  def insert(username:String,userTweet:String) = {
-    twitter.map(t=>(t.name,t.tweet)) += (username,userTweet)
-  }
-}
 object Util {
   val config = new twitter4j.conf.ConfigurationBuilder()
-    .setOAuthConsumerKey(".." )
+    .setOAuthConsumerKey("..")
     .setOAuthConsumerSecret("..")
     .setOAuthAccessToken("..")
     .setOAuthAccessTokenSecret("..")
     .build
 
-  def simpleStatusListener = new StatusListener() {
-    def onStatus(status: Status) ={
-      //      println(DataObjectFactory.getRawJSON(status))
-      println(status.getText +"-----"+ status.getUser+"==="+status.getId)
 
-      val ob=new TwitterDetails
-      Await.result(DatabaseConnection.db.run(ob.insert(status.getUser.getScreenName,status.getText)), Duration.Inf)
+  def simpleStatusListener = new StatusListener() {
+    def onStatus(status: Status) = {
+      //      println(DataObjectFactory.getRawJSON(status))
+      println(status.getText + "-----" + status.getUser + "===" + status.getId)
+      //
+      val ob = new TwitterDetails
+      Await.result(ob.insert(status.getUser.getScreenName, status.getText), Duration.Inf)
     }
+
     def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
+
     def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
-    def onException(ex: Exception) { ex.printStackTrace }
+
+    def onException(ex: Exception) {
+      ex.printStackTrace
+    }
+
     def onScrubGeo(arg0: Long, arg1: Long) {}
+
     def onStallWarning(warning: StallWarning) {}
   }
 
--- src/test/scala/twitter/TweetTest.scala
+++ src/test/scala/twitter/TweetTest.scala
@@ -8,7 +8,7 @@ class TweetTest extends FunSuite{
   test("configuration testing") {
     val tweet = new Twitter
     val input = tweet.testFun
-    val output = -1
+    val output = 200
     assert(input === output)
   }
 }
