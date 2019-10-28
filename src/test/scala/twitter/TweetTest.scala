a8db430efba5e0151f69d79463677cee2a564dc4
Sharanya <sharanya.m@techsophy.com>
2019-10-28 11:58:16 +0530

    first commit

---
+++ src/test/scala/twitter/TweetTest.scala
@@ -0,0 +1,14 @@
+package twitter
+
+import org.scalatest.FunSuite
+
+import scala.concurrent.Await
+
+class TweetTest extends FunSuite{
+  test("configuration testing") {
+    val tweet = new Twitter
+    val input = tweet.testFun
+    val output = -1
+    assert(input === output)
+  }
+}
